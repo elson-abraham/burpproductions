8c1190b3e10b516c71156a52679ba05f8e7f9bc8
elson-abraham <91924644+elson-abraham@users.noreply.github.com>
2023-04-08 19:47:20 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -45,6 +45,47 @@
       }
     });
     
+
+    //Slideshow
+    let slideIndex = 0;
+    let timeoutId = null;
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    showSlides();
+    function currentSlide(index) {
+      slideIndex = index;
+      showSlides();
+    }
+    function plusSlides(step) {
+      if(step < 0) {
+        slideIndex -= 2;
+        if(slideIndex < 0) {
+          slideIndex = slides.length - 1;
+        }
+      }
+      showSlides();
+    }
+    
+    function showSlides() {
+      let i;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }    
+
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].classList.add('activeslide');
+      if(timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(showSlides, 3000); // Change image every 5 seconds
+    }
+
+    
   })(window.jQuery);
 
   
