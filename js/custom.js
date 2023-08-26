503ae1ba08d418734bf1478891aae0047aea0700
Elson Abrahm <91924644+elson-abraham@users.noreply.github.com>
2023-08-26 11:47:15 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,117 +1,89 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // AOS ANIMATIONS
-    AOS.init();
+  // AOS ANIMATIONS
+  AOS.init();
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").delay(500).slideUp("slow"); // set duration in brackets
+  });
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-    // NEWS IMAGE RESIZE
-    function NewsImageResize(){
-      $(".navbar").scrollspy({ offset: -76 });
-      
-      var LargeImage = $('.large-news-image').height();
+  // NEWS IMAGE RESIZE
+  function NewsImageResize() {
+    $(".navbar").scrollspy({ offset: -76 });
 
-      var MinusHeight = LargeImage - 6;
+    var LargeImage = $(".large-news-image").height();
 
-      $('.news-two-column').css({'height' : (MinusHeight - LargeImage / 2) + 'px'});
-    }
+    var MinusHeight = LargeImage - 6;
 
-    $(window).on("resize", NewsImageResize);
-    $(document).on("ready", NewsImageResize);
+    $(".news-two-column").css({ height: MinusHeight - LargeImage / 2 + "px" });
+  }
 
-    $('a[href*="#"]').click(function (event) {
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top - 66
-          }, 1000);
-        }
+  $(window).on("resize", NewsImageResize);
+  $(document).on("ready", NewsImageResize);
+
+  $('a[href*="#"]').click(function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 66,
+          },
+          1000
+        );
       }
-    });
-    
+    }
+  });
 
-    //SLIDESHOW IMAGE
-    let slideIndex = 0;
-    let timeoutId = null;
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
+  //SLIDESHOW IMAGE
+  let slideIndex = 0;
+  let timeoutId = null;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
 
+  showSlides();
+  function currentSlide(index) {
+    slideIndex = index;
     showSlides();
-    function currentSlide(index) {
-      slideIndex = index;
-      showSlides();
-    }
-    function plusSlides(step) {
-      if(step < 0) {
-        slideIndex -= 2;
-        if(slideIndex < 0) {
-          slideIndex = slides.length - 1;
-        }
-      }
-      showSlides();
-    }
-    
-    function showSlides() {
-      let i;
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }    
+  }
+  // function plusSlides(step) {
+  //   if(step < 0) {
+  //     slideIndex -= 2;
+  //     if(slideIndex < 0) {
+  //       slideIndex = slides.length - 1;
+  //     }
+  //   }
+  //   showSlides();
+  // }
 
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].classList.add('activeslide');
-      if(timeoutId) {
-        clearTimeout(timeoutId);
-      }
-      timeoutId = setTimeout(showSlides, 1500); // Change image every 1.5 seconds
+  function showSlides() {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
     }
 
-    //
-    // function passwd(){
-    //   var password = prompt('Enter the password');
-    //   if(password.toLowerCase() == "hereinBurpProductions7991"){
-    //     window.open("folder/history.zip")    
-    //   }else{
-    //     alert("incorrect password!! please try again");
-    //   }
-    // }
-
-
-    //CAPATCHA CHECH
-    // var allowSubmit = false;
-
-    // function capcha_filled () {
-    //   allowSubmit = true;
-    // }
-    // function capcha_expired () {
-    //   allowSubmit = false;
-    // }
-    
-    // function check_if_capcha_is_filled (e) {
-    //   if(allowSubmit) return true;
-    //   e.preventDefault();
-    //   alert('Fill in the capcha!');
-    // }
-    
-
-  })(window.jQuery);
-
-  
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("activeslide");
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(showSlides, 1000); // Change image every 1 seconds
+  }
+})(window.jQuery);
