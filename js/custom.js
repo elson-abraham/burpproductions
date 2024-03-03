2809cf750ebd45ce02a487154dbe8a2666fbfc65
Elson Abrahm <91924644+elson-abraham@users.noreply.github.com>
2024-03-03 15:52:25 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -87,4 +87,63 @@
     timeoutId = setTimeout(showSlides, 1000); // Change image every 1 seconds
   }
 
+	//Dark and Light mode Button
+  
+	/**
+ * Change the text of a button
+ * @param {el} object HTMLElement: button to change text of
+ * @param {dText} string: default text
+ * @param {nText} string: new text
+ **/
+	function changeText(el, dText, nText) {
+		var content = '',
+			context = '';
+
+		/**
+		 * Set the text of a button
+		 *     - dependant upon current text
+		 **/
+		function setText() {
+			return (content === dText) ? nText : dText;
+		}
+
+		/* Check to see if the browser accepts textContent */
+		if ('textContent' in document.body) {
+			context = 'textContent';
+			/* Get the current button text */
+			content = el[context];
+			/* Browser does NOT use textContent */
+		} else {
+			/* Get the button text with fallback option */
+			content = el.firstChild.nodeValue;
+		}
+
+		/* Set button text */
+		if (context === 'textContent') {
+			el.textContent = setText();
+		} else {
+			el.firstChild.nodeValue = setText();
+		}
+	}
+
+	var defaultText,
+		substituteText,
+		btn;
+
+	/* Default text of the button */
+	defaultText = 'Light Mode';
+	/* Alternate text for button */
+	substituteText = 'Dark Mode';
+	/* Grab our button */
+	btn = document.querySelector('.toggleMode');
+
+	/* Add a listener to the button instance so we can manipulate it */
+	btn.addEventListener('click', function () {
+		var element = document.body;
+		element.classList.toggle("light-mode");
+		
+		changeText(this, defaultText, substituteText);
+	}, false);
+
+
 })(window.jQuery);
